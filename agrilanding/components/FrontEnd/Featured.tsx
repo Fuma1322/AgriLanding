042039ed6181ec5172c6e9bc7f846e2afe53edeb
Fuma1322 <leonardfuma26@gmail.com>
2024-08-24 03:21:43 +0200

    Updates...03:22am

--- agrilanding/components/FrontEnd/Featured.tsx
+++ agrilanding/components/FrontEnd/Featured.tsx
@@ -28,45 +28,47 @@ const services = [
 export default function Feature() {
   return (
     <div>
-      <div className="py-14">
-        <div className="max-w-screen-xl mx-auto px-4 md:px-8">
-          <div className="max-w-xl mx-auto text-center">
-            <h3 className="text-[#204E51] text-3xl font-bold sm:text-4xl">
-              Our Featured Product
-            </h3>
-            <p className="mt-3 flex items-center justify-center">
-              Li Europan lingues es membres del sam familie. Lor separat existentie es un myth. Por scientie, musica.
-            </p>
-          </div>
-          <div className="mt-12 flex justify-center">
-            <ul className="inline-grid grid-cols-2 gap-x-10 gap-y-6 md:gap-x-16 md:grid-cols-3 lg:grid-cols-4">
-              {services.map((service, idx) => (
-                <li key={idx}>
-                  <motion.div
-                    className="h-full rounded-2xl bg-white p-4 border border-neutral-200 shadow-xl flex flex-col items-center justify-center"
-                  >
-                    <div className="text-2xl rounded-full px-2 py-0.5 mt-4">
-                      <Image
-                        src={service.imageSrc}
-                        alt={service.title}
-                        width={64}
-                        height={64}
-                        className="rounded-lg"
-                      />
-                    </div>
-                    <h4 className="text-lg font-semibold text-[#204E51] mt-4">
-                      {service.title}
-                    </h4>
-                    <p className="sm:text-sm lg:text-xl text-center text-neutral-500 mt-4">
-                      {service.description}
-                    </p>
-                  </motion.div>
-                </li>
-              ))}
-            </ul>
-          </div>
+    <div className="py-14">
+      <div className="max-w-screen-xl mx-auto px-4 md:px-8">
+        <div className="max-w-xl mx-auto text-center">
+          <h3 className="text-[#204E51] text-3xl font-bold sm:text-4xl">
+            Our Featured Product
+          </h3>
+          <p className="mt-3 flex items-center justify-center">
+            Li Europan lingues es membres del sam familie. Lor separat existentie es un myth. Por scientie, musica.
+          </p>
+        </div>
+        <div className="mt-12 flex justify-center">
+          <ul className="inline-grid grid-cols-2 gap-x-10 gap-y-6 md:gap-x-16 md:grid-cols-3 lg:grid-cols-4">
+            {services.map((service, idx) => (
+              <li key={idx}>
+                <motion.div
+                  className="h-full rounded-2xl bg-white p-4 border border-neutral-200 shadow-xl flex flex-col items-center justify-center"
+                >
+                  <div className="w-full h-auto overflow-hidden rounded-lg">
+                    <Image
+                      src={service.imageSrc}
+                      alt={service.title}
+                      width={100}
+                      height={100} 
+                      layout="responsive"
+                      objectFit="cover"
+                      className="rounded-lg"
+                    />
+                  </div>
+                  <h4 className="text-lg font-semibold text-[#204E51] mt-4">
+                    {service.title}
+                  </h4>
+                  <p className="sm:text-sm lg:text-xl text-center text-neutral-500 mt-4">
+                    {service.description}
+                  </p>
+                </motion.div>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </div>
+  </div>
   );
 }
