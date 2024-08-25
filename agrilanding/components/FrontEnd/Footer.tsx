81103e37c99d4e1e342d45e03784fdc7e5970ffd
Fuma1322 <leonardfuma26@gmail.com>
2024-08-26 00:38:21 +0200

    footer finally fixed,

--- agrilanding/components/FrontEnd/Footer.tsx
+++ agrilanding/components/FrontEnd/Footer.tsx
@@ -1,65 +1,146 @@
-import Link from "next/link";
-import { Button } from "../ui/button";
-import { Github, Twitter, Facebook, Instagram } from "lucide-react";
+"use client"
 
-const buttons = [
-  {
-    icons: <Instagram className="text-[#204E51]" />,
-  },
-  {
-    icons: <Twitter className="text-[#204E51]" />,
-  },
-  {
-    icons: <Facebook className="text-[#204E51]" />,
-  },
-  {
-    icons: <Github className="text-[#204E51]" />,
-  },
-]
+import { Facebook, Github, Instagram, Twitter } from "lucide-react"
+import Link from "next/link"
 
-export default function Footer() {
+export default function Footer () {
+
+  const footerNavs = [
+    {
+      label: "Pages",
+      items: [
+          {
+              href: 'javascript:void()',
+              name: 'Home'
+          },
+          {
+              href: 'javascript:void()',
+              name: 'About'
+          },
+          {
+              href: 'javascript:void()',
+              name: 'Product'
+          },
+          {
+              href: 'javascript:void()',
+              name: 'Blog'
+          },
+      ],
+  },  
+      {
+          label: "About",
+          items: [
+              {
+                  href: 'javascript:void()',
+                  name: 'Testimonials'
+              },
+              {
+                  href: 'javascript:void()',
+                  name: 'Our Service'
+              },
+              {
+                  href: 'javascript:void()',
+                  name: 'Contact Us'
+              },
+              {
+                  href: 'javascript:void()',
+                  name: 'Benefit'
+              },
+          ]
+      },
+      {
+        label: "",
+        items: [
+            {
+                href: 'javascript:void()',
+                name: ''
+            },
+            {
+                href: 'javascript:void()',
+                name: ''
+            },
+            {
+                href: 'javascript:void()',
+                name: ''
+            },
+            {
+                href: 'javascript:void()',
+                name: ''
+            },
+        ],
+    },
+  ]
+
   return (
-    <footer className="bg-[#204E51] lg:py-32 sm:py-44">
-      <section>
-        <div className="grid md:grid-cols-[1.5fr_0.5fr_0.5fr] lg:ml-72">
-          <div className="flex flex-col gap-4">
-            <Link href="/">
-                <h2 className="font-bold text-3xl text-white">AgriVista Farms</h2>
-            </Link>
-            <p>
-              <p className="text-white">
-              Li Europan lingues es membres del sam <br/>
-              familie. Lor separat existentie 
-              </p>
-            </p>
+      <footer className="text-white bg-[#204E51] py-20 mx-auto md:px-8">
+          <div className="gap-6 justify-between md:flex">
+              <div className="flex-1">
+                  <div className="max-w-xs">
+                  <Link href="/">
+                      <h2 className="font-bold text-xl">Agrivista Farms</h2>
+                  </Link>
+                      <p className="leading-relaxed mt-6 text-[15px]">
+                      Li Europan lingues es membres del sam <br/> familie. Lor separat existentie 
+                      </p>
+                  </div>
+              </div>
+              <div className="flex-1 mt-5 space-y-2 items-center justify-between sm:flex md:space-y-0 md:mt-0">
+                  {
+                      footerNavs.map((item, idx) => (
+                          <ul
+                              className="space-y-4"
+                              key={idx}
+                          >
+                              <h4 className="text-white font-bold text-lg">
+                                  { item.label }
+                              </h4>
+                              {
+                                  item.items.map(((el, idx) => (
+                                      <li key={idx}>
+                                          <a 
+                                              href={el.href}
+                                              className="hover:underline"
+                                          
+                                          >
+                                              { el.name }
+                                          </a>
+                                      </li>
+                                  )))
+                              }
+                          </ul>
+                      ))
+                  }
+              </div>
           </div>
-          <div className="flex flex-col gap-4">
-            <h3 className="font-bold text-white">Pages</h3>
-            <Link href="/" className="text-white">Home</Link>
-            <Link href="/" className="text-white">About</Link>
-            <Link href="/" className="text-white">Product</Link>
-            <Link href="/" className="text-white">Blog</Link>
+          <div className="items-center justify-between sm:flex">
+              <div className="mt-2 sm:mt-0">
+                  <ul className="flex items-center space-x-4">
+                      <li className="w-10 h-10 border rounded-xl bg-white flex items-center justify-center">
+                          <Link href="javascript:void()">
+                              <Instagram className="text-[#204E51]" />
+                          </Link>
+                      </li>
+
+                      <li className="w-10 h-10 border rounded-xl bg-white flex items-center justify-center">
+                          <Link href="javascript:void()">
+                             <Twitter className="text-[#204E51]" />
+                          </Link>
+                      </li>
+
+                      <li className="w-10 h-10 border rounded-xl bg-white flex items-center justify-center">
+                          <Link href="javascript:void()">
+                              <Facebook className="text-[#204E51]" />
+                          </Link>
+                      </li>
+
+                      <li className="w-10 h-10 border rounded-xl bg-white flex items-center justify-center">
+                          <Link href="javascript:void()">
+                              <Github className="text-[#204E51]" />
+                          </Link>
+                      </li>
+                  </ul>
+              </div>
           </div>
-          <div className="flex flex-col gap-4">
-            <h5 className="font-bold text-white">About</h5>
-            <Link href="/" className="text-white">Testimonials</Link>
-            <Link href="/" className="text-white">Our Service</Link>
-            <Link href="/" className="text-white">Contact Us</Link>
-            <Link href="/" className="text-white">Benefit</Link>
-          </div>
-        </div>
-        <div className="flex flex-col justify-between gap-6 md:flex-row md:items-center md:gap-2">
-          <div className="flex gap-2 lg:ml-72">
-          {buttons.map((items,idx) => {
-            return (
-            <Button key={idx} variant="outline" size="icon">
-              {items.icons}
-            </Button>
-            )
-          })}
-          </div>
-        </div>
-      </section>
-    </footer>
-  );
+      </footer>
+  )
 }
